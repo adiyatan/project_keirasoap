54fcddd0bda786895d0e9b6806f2ef3aa10c94c0
adiyatan <adiyazam03@gmail.com>
2023-10-12 13:33:04 +0700

    fix css

--- registrasi.php
+++ registrasi.php
@@ -6,11 +6,9 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
   $id = $_COOKIE['id'];
   $key = $_COOKIE['key'];
 
-  //ambil username berdasarkan id
   $result = mysqli_query($conn, "SELECT username FROM user WHERE id ='$id'");
   $rows = mysqli_fetch_assoc($result);
 
-  //cek cookie dan id
   if ($key === hash('sha256', $rows['username'])) {
     $_SESSION['login'] = true;
   }
@@ -31,7 +29,7 @@ if (isset($_SESSION['login'])) {
 if (isset($_POST['register'])) {
   if (registrasi($_POST) > 0) {
     echo "<script>
-                alert('Berhasil mendaftar!');
+                alert('Registration successful!');
                 document.location.href = 'login.php'
               </script>";
   } else {
@@ -43,7 +41,7 @@ if (isset($_POST['register'])) {
 <html>
 
 <head>
-  <title>Registrasi Keira Health Portal</title>
+  <title>Register for Keira Health Portal</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <style>
     body {
@@ -113,34 +111,34 @@ if (isset($_POST['register'])) {
 
 <body>
   <div class="panel_login">
-    <p class="tulisan_atas fs-2">Silahkan Daftar</p>
+    <p class="tulisan_atas fs-2">Register for Keira Health Portal</p>
     <form action="" method="post">
       <div class="form-floating mb-5">
-        <input type="text" class="form-control fs-5" id="floatingInput" placeholder="Nama Lengkap" name="nama_user" style="height: 50px" required autocomplete="off">
-        <label for="floatingInput">Nama Lengkap :</label>
+        <input type="text" class="form-control fs-5" id="floatingInput" placeholder="Full Name" name="nama_user" style="height: 50px" required autocomplete="off">
+        <label for="floatingInput">Full Name:</label>
       </div>
 
       <div class="form-floating mb-5">
         <input type="text" class="form-control fs-5" id="floatingInput" placeholder="Username" name="username" style="height: 50px" required autocomplete="off">
-        <label for="floatingInput">Username :</label>
+        <label for="floatingInput">Username:</label>
       </div>
 
       <div class="form-floating mb-5">
         <input type="email" class="form-control fs-5" id="floatingInput" placeholder="test@example.com" name="email" style="height: 50px" required autocomplete="off">
-        <label for="floatingInput">Email :</label>
+        <label for="floatingInput">Email:</label>
       </div>
 
       <div class="form-floating mb-5">
         <input type="password" class="form-control fs-5" id="floatingInput" placeholder="Password" name="password" style="height: 50px" required autocomplete="off">
-        <label for="floatingInput">Password :</label>
+        <label for="floatingInput">Password:</label>
       </div>
 
       <div class="form-floating mb-5">
-        <input type="password" class="form-control fs-5" id="floatingInput" placeholder="Konfirmasi Password" name="password2" style="height: 50px" required autocomplete="off">
-        <label for="floatingInput">Konfirmasi Password :</label>
+        <input type="password" class="form-control fs-5" id="floatingInput" placeholder="Confirm Password" name="password2" style="height: 50px" required autocomplete="off">
+        <label for="floatingInput">Confirm Password:</label>
       </div>
 
-      <p style="margin-right: 180px; margin-top: -20px;">Have an account? <a href="login.php" style="color: #00a4a9; font-weight: bold;">Login</a>
+      <p style="margin-right: 130px; margin-top: -20px;">Already have an account? <a href="login.php" style="color: #00a4a9; font-weight: bold;">Login</a>
       </p>
 
       <input type="submit" class="tombol_regis" value="Register" name="register" style="color: black; background-color: #0DCAF0;">
